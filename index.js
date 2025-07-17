36c597dee386318ecd3b0ff8db9b4b8a7491ebc3
Emilio <al03044367>
2025-07-17 17:33:32 -0600

    si5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import './resources/css/cards.css'
 import './resources/js/memorama.js'
 
 
-import Alpine from './node_modules/alpinejs/dist/cdn.min.js'
+import Alpine from 'alpinejs'
 window.Alpine = Alpine
 Alpine.start()
 
